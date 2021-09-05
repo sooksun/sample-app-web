a2e5115c7fd30c0fde02ef40eaa1857ec0f7b248
School e office <sooksun2511@gmail.com>
2021-09-05 18:39:50 +0700

    Update index.js

--- CDN/js/index.js
+++ CDN/js/index.js
@@ -17,14 +17,14 @@ function websdkready() {
   // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
   ZoomMtg.preLoadWasm(); // pre download wasm file to save time.
 
-  var API_KEY = "YOUR_API_KEY";
+  var API_KEY = "TYaSd4wlTq23UiGySP3QrA";
 
   /**
    * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
    * The below generateSignature should be done server side as not to expose your api secret in public
    * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
    */
-  var API_SECRET = "YOUR_API_SECRET";
+  var API_SECRET = "DRcKoW7GiMyilRlf1XkGMD3k3riajmSNODUa";
 
   // some help code, remember mn, pwd, lang to cookie, and autofill.
   document.getElementById("display_name").value =
